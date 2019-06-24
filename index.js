2be87effd063c28d54f02037a537dcb91532baf8
ObaldThePedro <pedro.nunes.loureiro@gmail.com>
2019-06-24 11:00:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    for (i=0; i < names.length; i++) {
+      const thankYouCards = [];
+      thankYouCards.push(`Thank you ${names[i]} for the ${event} gift!`);
+    }
+  
+     return thankYouCards;
+  }
+
+  function countdown(i){
+      while(i >= 0){
+          console.log(i);
+          i--;
+      }
+  }
